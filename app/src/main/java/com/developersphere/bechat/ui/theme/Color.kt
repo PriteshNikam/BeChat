cf9595d29a5105319f32f028e1aec87d3a612c88
Pritesh Nikam <priteshnikam8121@gmail.com>
2025-07-19 19:20:20 +0530

    implement bluetooth permission screen ui

--- app/src/main/java/com/developersphere/bechat/ui/theme/Color.kt
+++ app/src/main/java/com/developersphere/bechat/ui/theme/Color.kt
@@ -2,21 +2,13 @@ package com.developersphere.bechat.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
-
 
 //custom color
 
 // Primary & Accent
-val BluePrimary = Color(0xFF1E88E5)
+val BluePrimary = Color(0xFF76A5D6)
 val BlueLight = Color(0xFF90CAF9)
-val BlueDark = Color(0xFF1565C0)
+val BlueDark = Color(0xFF000000) // 0xFF1565C0
 
 // Background & Surface
 val BackgroundLight = Color(0xFFF5F5F5)
